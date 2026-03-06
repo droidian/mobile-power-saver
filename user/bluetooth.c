a6e0e0405e06e2bf338e857f9e6a259235289bcc
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2026-03-06 18:16:45 +0100

    user: bluetooth: Show boolean as int

--- user/bluetooth.c
+++ user/bluetooth.c
@@ -417,8 +417,7 @@ bluetooth_set_powersave (Bluetooth *self,
                    can_powersave (self) &&
                    g_list_length (self->priv->connected) == 0;
 
-    // Crash on PMOS, WTF ?
-    //g_message ("Set Bluetooth powersave: %b", do_powersave);
+    g_message ("Set Bluetooth powersave: %i", do_powersave);
     if (do_powersave) {
         set_powersave (self, TRUE);
         set_services_powersave (self, TRUE);
